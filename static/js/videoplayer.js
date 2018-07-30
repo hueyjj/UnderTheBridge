fa23f84b2339ed9c4debfe48ca1ebb6c1c78ba92
hueyjj <jasperjeng@gmail.com>
2018-07-30 00:56:18 -0700

    Set torrent id and dynamically load user's torrent magnet id

--- static/js/videoplayer.js
+++ static/js/videoplayer.js
@@ -10,14 +10,19 @@ document.querySelector("#start").addEventListener("click", function (event) {
 
     client = new WebTorrent();
 
-    torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
+    //torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
+    let torrentInputContainer = document.querySelector("#torrent-input-box");
+    torrentId = torrentInputContainer.value;
+    console.log(torrentId);
 
     client.add(torrentId, function (torrent) {
         torrent.on("wire", function (wire, addr) {
             peers.push(addr);
         });
         var file = torrent.files.find(function (file) {
-            return file.name.endsWith('.mp4');
+            console.log("Found file for: ");
+            console.log(file.name);
+            return file.name;
         })
         file.appendTo('.video-container', function () {
             document.querySelector("video").volume = 0.5;
